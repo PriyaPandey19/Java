f59072810c7415047dc48f68f9796fa399ab6b24
PriyaPandey19 <pp479021@gmail.com>
2025-04-27 21:38:27 +0530

    tracking

--- BackTracking.java
+++ BackTracking.java
@@ -37,11 +37,83 @@ public class BackTracking {
         findSubsets(str, i+1, ans);
     }
 
-    
+    public static void findPermutation(String str, String ans){      //find all the permutation 
+        //base case
+        if(str.length() == 0){
+         System.out.println(ans);
+         return;
+        }
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            //"abcde" = "ab"+ "de" = abde
+              String Newstr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(Newstr, ans+curr);
+        }
+    }
+
+
+
+    public static boolean isSafe(char board[][],int row,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'Q'){
+              return false;
+            }
+        }
+
+        //diagonal left up
+        for(int i=row-1,j=col-1; i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        //diagonal right up
+        for(int i=row-1,j=col+1; i>=0 && j<board.length;i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void nQueens(char board[][],int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        //column loop
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)) {
+                board[row][j] ='Q';
+                nQueens(board, row+1);    //function call
+                board[row][j] ='.';       //backtracking step
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]){
+        System.out.println("------------chess board-----------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
 
 
     public static void main(String[] args){
-        String str = "abc";
-        findSubsets(str, 0, "");
-    }
+        int n = 4   ;
+        char board[][] = new char[n][n];
+        //initialize
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]= '.';
+            }
+        }
+        nQueens(board, 0);
+    }   
 }
