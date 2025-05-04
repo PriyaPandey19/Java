fe1a33c642c43c88e63a73025a680f03bfa64ca3
PriyaPandey19 <pp479021@gmail.com>
2025-05-04 22:20:38 +0530

    hy

--- LinkedListImplementationt.java
+++ LinkedListImplementationt.java
@@ -239,6 +239,9 @@ public class LinkedListImplementationt {
             return false;
         }
 
+
+
+
         public static void removeCycle(){          //remove a cycle
             //detect cycle
             Node slow = head;
@@ -270,7 +273,11 @@ public class LinkedListImplementationt {
              prev.next = null;
         }
 
-        private Node getMid(Node head){
+
+
+
+
+        private Node getMid(Node head){                    //get mid for merge function
             Node slow = head;
             Node fast = head.next;
 
@@ -281,23 +288,99 @@ public class LinkedListImplementationt {
             return slow;
         }
 
-        // public Node mergeSort(Node head){
-        //     if(head == null && head.next != null){
-        //         return head;
-        //     }
-        //    //find mid
-        //    Node mid = getMid();
-        //    //left & right MS
-        //    Node rightHead = mid.next;
-        //    mid.next= null;
-        //    Node newLeft = mergeSort(head);
-        //    Node newRight = mergeSort(rightHead);
+        private Node merge(Node head1, Node head2){              //helper function of merge
+            Node mergedLL = new Node(-1);
 
-        //    return merge(newLeft,newRight);
-        // }
+            Node temp = mergedLL;
+
+            while(head1 != null && head2 != null){
+                if(head1.data <= head2.data){    //if head1 is small add it in merge LL
+                    temp.next = head1;
+                    head1 = head1.next;
+                    temp = temp.next;
+                }
+                else{
+                    temp.next = head2;   //if head2 is small add it in merge LL
+                    head2 = head2.next;
+                    temp = temp.next;  
+                }
+            }
+            while(head1 != null){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }
+            while (head2 != null){
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;    
+            }
+            return mergedLL.next;
+        }
+
+        public void zigZag(){
+            //find mid
+            Node slow = head;
+            Node fast = head.next;
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            Node mid = slow;
+
+            //reverse 2nd half
+            Node curr = mid.next;
+            mid.next= null;
+            Node prev= null;
+            Node next;
+
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            Node left = head;
+            Node right= prev;
+            Node nextL,nextR;
+
+            //alt merge zig zag merge
+            while(left != null && right != null){
+                nextL = left.next;
+                left.next = right;
+                nextR = right.next;
+                right.next = nextL;
+
+                left = nextL;
+                right = nextR;
+            }
+        }
+
+
+
+        public Node mergeSort(Node head){                    //merge sort using LL
+            if(head == null || head.next == null){
+                return head;
+            }
+           //find mid
+           Node mid = getMid(head);
+           //left & right MS
+           Node rightHead = mid.next;
+           mid.next= null;
+           Node newLeft = mergeSort(head);
+           Node newRight = mergeSort(rightHead);
+
+           return merge(newLeft,newRight);
+        }
+
+
+
+
+
 
     public static void  main(String args[]){
-    // LinkedListImplementationt ll = new LinkedListImplementationt();
+     LinkedListImplementationt ll = new LinkedListImplementationt();
     // head = new Node(1);
     // Node temp = new Node(2);
     // head.next = temp;
@@ -307,11 +390,16 @@ public class LinkedListImplementationt {
     // System.out.println(isCycle());
     // removeCycle();
     // System.out.println(isCycle());
-    LinkedList<Integer> ll = new LinkedList<>();
+    //LinkedList<Integer> ll = new LinkedList<>();
     ll.addLast(1);
     ll.addLast(2);
-    ll.addFirst(0);
-    System.out.println(ll);
+    ll.addLast(3);
+    ll.addLast(4);
+    ll.addLast(5);
+    
+    ll.print();
+    ll.zigZag();
+    ll.print();
    
     }
 }
