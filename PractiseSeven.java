757c8637589a0b844a9b7cbb428bfc09812f37ae
PriyaPandey19 <pp479021@gmail.com>
2025-05-30 22:40:08 +0530

    bye

--- PractiseSeven.java
+++ PractiseSeven.java
@@ -1,8 +1,76 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public class PractiseSeven {
 
     
 
     public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+     list.add(6);
+     list.add(2);
+     list.add(4);
+     System.out.println(list);
+     
+
+     
+
+
+
+
+    //  ListIterator<String> it = list.listIterator();
+    //  while(it.hasNext()){
+    //  String data = it.next();
+    //  if(data.length() <= 3){
+    //     it.set("bye");
+    //  } 
+    //  }
+    //  System.out.println(list);
+     
+
+    //   while (it.hasPrevious()) {
+    //      String prevData= it.previous();
+    //      if(it.nextIndex() == 0){   
+        
+    //      }
+    //      else{
+    //         it.set("hello");
+    //      }
+    // }
+    // System.out.println(list);
+
+
+
     
+
+
+
+
+//      ArrayList<String> list2 = new ArrayList<>();
+//      list2.add("ranul");
+//      list2.add("shanti");
+//      list2.add("mohit");
+//      list2.add("rohit");
+   
+
+//      ListIterator<String> li = list2.listIterator();
+//      while(li.hasNext()){
+//    String data = li.next();
+//    System.out.println(data);
+//      }
+//      System.out.println("alag hu  ");
+//      while (li.hasPrevious()) {
+//         String prevData= li.previous();
+//         System.out.println(prevData);
+//      }
+
+
+
+
+
     }
 }
