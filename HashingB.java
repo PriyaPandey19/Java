51d0700da16ceea9171fd6e2170aac8442225aaf
PriyaPandey19 <pp479021@gmail.com>
2025-06-04 20:57:08 +0530

    trie

--- HashingB.java
+++ HashingB.java
@@ -95,6 +95,44 @@ public class HashingB {
     }
 
 
+     public static  void largestSubarraySum(int arr[]){          //largest subarray with sum 0
+     HashMap<Integer,Integer> map = new HashMap<>();
+     int len =0;
+     int  sum =0;
+
+     for(int j=0;j<arr.length;j++){
+        sum += arr[j];
+        if(map.containsKey(sum)){
+            len = Math.max(len, j-map.get(sum));
+        }
+        else{
+            map.put(sum,j);
+        }
+     }
+     System.out.println("largest subarray with sum 0: "+ len);
+
+     }
+
+
+     public static void subarrayWithEqualTok(int arr[],int k){       //subarray sum equal to k
+            HashMap<Integer,Integer>map = new HashMap<>();
+            map.put(0,1);
+
+            int sum =0;
+            int ans =0;
+
+            for(int j=0;j<arr.length;j++){
+                sum += arr[j];
+                if(map.containsKey(sum -k)){
+                    ans += map.get(sum -k);
+                }
+                map.put(sum, map.getOrDefault(sum, 0)+1);
+            }
+            System.out.println(ans);
+     }
+
+
+
     
 
 
@@ -103,6 +141,15 @@ public class HashingB {
 
 
     public static void main(String[] args) {
+        int arr1[]={10,2,-2,-20,10};     //main function of subarray with sum equal to k
+        int k = -10;
+        subarrayWithEqualTok(arr1, k);
+
+
+
+
+        int arr[] = {15,-2,2,-8,1,7,10,23};     //main function of subarray sum equal to 0
+        largestSubarraySum(arr);
     //  int arr[]= {1,3,2,5,1,3,1,5,1};
     //  majorityElements(arr); 
     //  String s = "race";
@@ -112,7 +159,7 @@ public class HashingB {
     //  int arr1[] ={7,3,9};
     //  int arr2[] ={6,3,9,2,9,4};
     //  interSectionUnion(arr1, arr2);
-    HashMap<String,String> tickets = new HashMap<>();
+    HashMap<String,String> tickets = new HashMap<>();    //main function of get path
     tickets.put("Chennai","Bengaluru");
     tickets.put("Mumbai","Delhi");
     tickets.put("Goa","Chennai");
--- PractiseEight.java
+++ PractiseEight.java
@@ -44,6 +44,51 @@ class DownloadFile extends Thread{             //with thread
 
 }
 
+class MyRunnable implements Runnable{
+   String file;
+
+    public MyRunnable(String file){
+        this.file = file;
+    }
+    public  void run(){
+     for(int i=1;i<=5;i++){
+        System.out.println(file + " downloading.."+(i*20) + "%");
+     
+     try{
+       Thread.sleep(2000);
+     }catch(Exception e){}
+     }
+     System.out.println(file + " done");
+    }
+}
+
+
+
+class bank extends Thread {
+    int amt;
+    static int balance=1000;
+
+    public bank(int amt) {
+        this.amt = amt;
+      
+    }
+
+    public void run() {
+      synchronized(bank.class){
+        if (balance >= amt) {
+            
+            System.out.println("is going withdraw "+amt);
+        
+        balance -=amt;
+        System.out.println("remaining balance "+ balance);
+      }
+      else{
+        System.out.println("sorry....");
+      }
+    }
+    }
+}
+
 
 
 
@@ -54,12 +99,33 @@ public class PractiseEight {
     //  t1.start();
     //  for(int i=0;i<5;i++){
     //  System.out.println("hiii");} 
-     DownloadFile d1 = new DownloadFile("file A");
-     DownloadFile d2 = new DownloadFile("file B");
-     d1.start(); 
-     d2.start();
+    //  DownloadFile d1 = new DownloadFile("file A");
+    //  DownloadFile d2 = new DownloadFile("file B");
+    //  d1.start(); 
+    //  d2.start();
+// MyRunnable obj = new MyRunnable("file A");
+// MyRunnable obj1 = new MyRunnable("file B");
+// Thread t1 = new Thread(obj);
+// Thread t2 = new Thread(obj1);
+// t1.start();
+// t2.start();
+// System.out.println("Main thread is running");\
+// MyRunnable obj = new MyRunnable("file A");
+// Thread t1 = new Thread();
+// t1.start();
+// try{
+//   t1.join();
+// }catch(InterruptedException e){
+//   e.printStackTrace();
+// }
+// for(int i=1;i<5;i++){
+//   System.out.println(i);
+// }
 
-
+bank b1 = new bank(800);
+b1.start();
+bank b2 = new bank(500);
+b2.start();
 
     }
 }
