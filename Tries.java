6a9b87ef25f0026a55a5f360a1220e1498e48c90
PriyaPandey19 <pp479021@gmail.com>
2025-06-07 22:52:50 +0530

    trie

--- Tries.java
+++ Tries.java
@@ -5,7 +5,7 @@ public class Tries {
         Node children[] = new Node[26];
         boolean eow = false;
         int freq;
-
+      
         public Node(){
             for(int i=0;i<children.length;i++){
                 children[i] = null;
@@ -85,6 +85,45 @@ public class Tries {
     }
 
 
+    public static int countNodes(Node root){   //count unique substring
+      if(root == null){
+        return 0;
+      }
+      int count =0;
+      for(int i=0;i<26;i++){
+        if(root.children[i] != null){
+          count += countNodes(root.children[i]);
+        }
+      }
+      return count+1;
+    }
+
+
+    public static String ans = "";
+    public static void longestWord(Node root, StringBuilder temp){ //longest word with all prefixex
+      if(root == null){
+        return;
+      }
+
+      for(int i=0;i<26;i++){
+        if(root.children[i] != null && root.children[i].eow ==true){
+          char ch = (char)(i +'a'); 
+          temp.append(ch);
+          if(temp.length() > ans.length()){
+            ans = temp.toString();
+          }
+          longestWord(root.children[i], temp);
+          temp.deleteCharAt(temp.length()-1);  //backtracking
+        }
+      }
+    }
+
+
+
+
+
+
+
 
   public static void main(String[] args) {
   // String words[] ={"the","a","there","their","any","three"};
@@ -93,6 +132,8 @@ public class Tries {
   // } 
   // System.out.println(search("there"));
   // System.out.println(search("thor"));
+
+
 
   // String arr[] = {"i","like","sam","samsung","mobile","ice"};
   // for(int i=0;i<arr.length;i++){
@@ -108,13 +149,36 @@ public class Tries {
   //  }
   //  root.freq = -1;
   //  findPrefix(root, "");
-  String words[] ={"apple","app","mango","man","women"};
-  String prefix1 = "app";
-  String prefix2 ="moon";
 
+
+
+  // String words[] ={"apple","app","mango","man","women"};
+  // String prefix1 = "app";
+  // String prefix2 ="moon";
+
+  // for(int i=0;i<words.length;i++){
+  //   insert(words[i]);
+  // }
+  // System.out.println(startsWith(prefix1));
+
+
+  // String str = "ababa";
+  // for(int i=0;i<str.length();i++){
+  //   String suffix = str.substring(i);
+  //   insert(suffix);
+  // }
+  // System.out.println(countNodes(root));
+
+
+
+  String words[] ={"a","banana","app","appl","ap","apply","apple"};
   for(int i=0;i<words.length;i++){
     insert(words[i]);
   }
-  System.out.println(startsWith(prefix1));
+  longestWord(root, new StringBuilder(""));
+  System.out.println(ans);
+
+
+
   }  
 }
