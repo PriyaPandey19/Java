77a5ea693ef499334a35c4b93b895a2efb80e546
PriyaPandey19 <pp479021@gmail.com>
2025-06-12 12:01:12 +0530

    hy

--- Graph.java
+++ Graph.java
@@ -17,28 +17,25 @@ public class Graph {
       for(int i=0;i<graph.length;i++){       //intitilize the empty  arrayList
       graph[i] = new ArrayList<>();
       }  
-      //0 vertex
-      graph[0].add(new Edge(0, 1));
-      graph[0].add(new Edge(0, 2));
-       
-
-     //1 vertex
-     graph[1].add(new Edge(1,0));
-     graph[1].add(new Edge(1, 3));
+      
+    
     
      //2 vertex
-     graph[2].add(new Edge(2, 0));
-     graph[2].add(new Edge(2, 4));
-     
+     graph[2].add(new Edge(2, 3));
+    
 
      //3 vertex
      graph[3].add(new Edge(3, 1));
-     graph[3].add(new Edge(3, 4));
-    
+
      //4 vertex
-     graph[4].add(new Edge(4, 2));
-     graph[4].add(new Edge(4, 3));
-    
+     graph[4].add(new Edge(4, 0));
+     graph[4].add(new Edge(4, 1));
+
+     //5 vertex
+     graph[5].add(new Edge(5, 0));
+     graph[5].add(new Edge(5, 2));
+
+
     }
 
     public static void bfs(ArrayList<Edge>[] graph){
@@ -186,15 +183,80 @@ public class Graph {
 
 
 
+
+    public static boolean isCycle(ArrayList<Edge>[] graph){         //detect cycle in directed graph
+        boolean vis[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i]){
+                if(isCycleUtil(graph,i,vis,stack)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+   public static boolean isCycleUtil(ArrayList<Edge>[] graph,int curr,boolean vis[],boolean stack[]){
+    vis[curr] = true;
+    stack[curr] = true;
+
+    for(int i=0;i<graph[curr].size();i++){
+        Edge e = graph[curr].get(i);
+        if(stack[e.dest]){
+            return true;
+        }
+        if(!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)){
+            return true;
+        }
+    }
+    stack[curr]= false;
+    return false;
+   } 
+
+
+
+
+
+
+
+   public static void topSort(ArrayList<Edge>[] graph){    //topological sort using DFS
+    boolean vis[] = new boolean[graph.length];
+    Stack<Integer> s = new Stack<>();
+
+    for(int i=0;i<graph.length;i++){
+        if(!vis[i]){
+            topSortUtil(graph,i,vis,s);
+        }
+    }
+    while (!s.isEmpty()) {
+        System.out.print(s.pop() + " ");
+    }
+   }
+
+   public static void topSortUtil(ArrayList<Edge>[] graph,int curr,boolean vis[],Stack<Integer> s){
+    vis[curr] = true;
+
+    for(int i=0;i<graph[curr].size();i++){
+        Edge e = graph[curr].get(i);
+        if(!vis[e.dest]){
+            topSortUtil(graph,e.dest,vis,s);
+        }
+    }
+    s.push(curr); 
+   }
     
 
 
     public static void main(String[] args) {
-      int V = 5;
+      int V = 6;
       ArrayList<Edge>[] graph = new ArrayList[V];
-      createGraph(graph);
-      System.out.println(detectCycle(graph));
-      System.out.println(isBipartitieGraph(graph));
+       createGraph(graph);
+    //   System.out.println(isCycle(graph));
+      topSort(graph);
+    //   System.out.println(detectCycle(graph));
+    //   System.out.println(isBipartitieGraph(graph));
     // System.out.println(hasPath(graph, 0, 5, new boolean[V]));
 
     }
