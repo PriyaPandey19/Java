68b1caed9e8f6d2017cfcccd91e0ed396b152e70
PriyaPandey19 <pp479021@gmail.com>
2025-06-14 20:49:39 +0530

    graph2

--- Graph2.java
+++ Graph2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Graph2 {
        static class Edge{
@@ -19,24 +20,26 @@ public class Graph2 {
       }  
       
     //0 vertex
-     graph[0].add(new Edge(0, 1,2));
-    graph[0].add(new Edge(0, 2,4)); 
-    
-     //1vertex
-     graph[1].add(new Edge(1, 2,-4));
+     graph[0].add(new Edge(0, 1,10));
+    graph[0].add(new Edge(0, 2,15));
+     graph[0].add(new Edge(0, 3,30));
      
     
-
+     //1vertex
+     graph[1].add(new Edge(1, 0,10));
+     graph[1].add(new Edge(1, 3,40));
+     
+     
      //2 vertex
-     graph[2].add(new Edge(2, 3,2));
+     graph[2].add(new Edge(2, 0,15));
+     graph[2].add(new Edge(2, 3,50));
+     
 
      //3 vertex
-     graph[3].add(new Edge(3, 4,4));
-    
+     graph[3].add(new Edge(3, 1,40));
+     graph[3].add(new Edge(3, 2,50));
+     
 
-     //4 vertex
-     graph[4].add(new Edge(4, 1,-1));
-   
 
     }
 
@@ -70,14 +73,49 @@ public class Graph2 {
     System.out.println();
     }
 
+    static class Pair implements Comparable<Pair>{
+     int v;
+     int cost;
+     public Pair(int v, int c){
+        this.v = v;
+        this.cost =c;
+     }
+     @Override
+     public int compareTo(Pair p2){
+        return this.cost - p2.cost;
+     }
+
+    }
+    public static void prims(ArrayList<Edge> graph[]){
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        int finalCost = 0;    //total min weight
+
+        while(!pq.isEmpty()){
+            Pair curr = pq.remove();
+            if(!vis[curr.v]){
+            vis[curr.v] = true;
+            finalCost +=curr.cost;
+
+            for(int i=0;i<graph[curr.v].size();i++){
+                Edge e = graph[curr.v].get(i);
+                pq.add(new Pair(e.dest, e.wt));
+            }
+            }
+        }
+        System.out.println("final(min) cost "+ finalCost);
+    }
+
 
 
 
 
     public static void main(String[] args) {
-     int V =5;
+     int V =4;
      ArrayList<Edge> graph[] = new ArrayList[V];
      createGraph(graph);
      bellmanFord(graph, 0);
+     prims(graph);
     }
 }
