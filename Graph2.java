4579c02c97942877e19cc8a324ff5b694c42b914
PriyaPandey19 <pp479021@gmail.com>
2025-06-16 20:00:42 +0530

    bye

--- Graph2.java
+++ Graph2.java
@@ -346,8 +346,24 @@ public static void kruskalsMST(ArrayList<Edge2>edges, int V){    //kruskal algo
 
 
 
+public void helper(int[][] image, int sr, int sc, int color,boolean vis[][],int orgCol){   //flood fill algo
+if(sr < 0|| sc <0 || sr >= image.length|| sc >= image[0].length|| vis[sr][sc] || image[sr][sc] != orgCol){
+    return;
+}
+image[sr][sc] = color;
 
+//left
+helper(image, sr, sc-1, color, vis, orgCol);
+helper(image, sr, sc+1, color, vis, orgCol);
+helper(image, sr-1, sc, color, vis, orgCol);
+helper(image, sr+1, sc, color, vis, orgCol);
 
+}
+public int[][] floodFill(int[][] image,int sr, int sc, int color){
+    boolean vis[][] = new boolean[image.length][image[0].length];
+    helper(image, sr,sc,color, vis, image[sr][sc]);
+    return image;
+}
 
 
 
