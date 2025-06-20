2aaee5f6b394b1eda3083570754f692481aeeb54
PriyaPandey19 <pp479021@gmail.com>
2025-06-20 21:10:29 +0530

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -120,7 +120,41 @@ public class DynamicProgramming {
         print(dp);
       return dp[n][W];
 
+    }
 
+     public static void print1(boolean dp[][]){
+            for(int i=0;i<dp.length;i++){
+                for(int j=0;j<dp[0].length;j++){
+                    System.out.print(dp[i][j] +" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+
+    public static boolean targetSumSubset(int arr[], int sum){    //target subset sum using tabulation
+        int n = arr.length;
+        boolean dp[][] = new boolean[n+1][sum+1];
+        //i = items & j = target sum
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = true;
+        }
+         for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                int v = arr[i-1];
+                //include
+                if( v <= j && dp[i-1][j-v] == true){
+                    dp[i][j] = true;
+                }
+                //exclude
+                else if(dp[i-1][j] == true){
+                    dp[i][j] = true;
+                }
+            }
+         }
+         print1(dp);
+         return dp[n][sum];
     }
     
 
@@ -131,17 +165,30 @@ public class DynamicProgramming {
 
 
     public static void main(String[] args) {
-    int val[] ={15, 14, 10,45, 30};
-    int wt[] ={2,5,1,3,4};
-    int W = 7;
-    int dp[][] = new int[val.length+1][W+1];
-    for(int i=0;i<dp.length;i++){
-        for(int j=0;j<dp[0].length;j++){
-            dp[i][j] = -1;
-        }
-    }
+
+
+//     int val[] ={15, 14, 10,45, 30};
+//     int wt[] ={2,5,1,3,4};
+//     int W = 7;
+//     int dp[][] = new int[val.length+1][W+1];
+//     for(int i=0;i<dp.length;i++){
+//         for(int j=0;j<dp[0].length;j++){
+//             dp[i][j] = -1;
+//         }
+//     }
    
-  //   System.out.println(knapsack(val, wt, W, val.length,dp)); 
-  System.out.println(knapsackTab(val, wt, W));
+//   //   System.out.println(knapsack(val, wt, W, val.length,dp)); 
+//   System.out.println(knapsackTab(val, wt, W));
+
+
+
+
+int arr[]= {4,2,7,1,3};
+int sum =10; 
+System.out.println(targetSumSubset(arr, sum));
+
+
+
+
     }
 }
