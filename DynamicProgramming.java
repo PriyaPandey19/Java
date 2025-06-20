158eaa2c449b3a262a24b76993b52043e0b5941d
PriyaPandey19 <pp479021@gmail.com>
2025-06-20 11:05:16 +0530

    bye

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -22,7 +22,7 @@ public class DynamicProgramming {
         return countWays(n-1)+countWays(n-2);
     }
 
-    public static int countWays(int n,int ways[]){      //using recursion
+    public static int countWays(int n,int ways[]){      //using memoization
         if(n ==0){
             return 1;
         }
@@ -36,12 +36,36 @@ public class DynamicProgramming {
         return ways[n];
     }
 
+     public static int countWays1(int n){      //using tabulation
+
+       int dp[] = new int[n+1];
+       dp[0] =1;
+
+       for(int i=1;i<=n;i++){
+        if(i==1){
+            dp[i] = dp[i-1]+0;
+
+        }
+        else{
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+       }
+       return dp[n];
+    }
+
+    
+
+    
+
+
+
+
 
     public static void main(String[] args) {
      int n = 5;
      int f[] = new int[n+1];
      //System.out.println(fibTabulation(n,f));  
     Arrays.fill(f,-1);
-     System.out.println(countWays(n,f)); 
+     System.out.println(countWays1(n)); 
     }
 }
