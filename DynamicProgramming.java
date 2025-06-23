05006a8bde8121bbfd3251b97f36643e26e1c831
PriyaPandey19 <pp479021@gmail.com>
2025-06-23 20:23:59 +0530

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -294,6 +294,35 @@ public class DynamicProgramming {
 
 
 
+  public static int longestCommonSubstring(String str3, String str4){    //lcs using tabulation
+    int n = str3.length();
+    int m = str4.length();
+    int ans =0; 
+
+    int dp[][] = new int[n+1][m+1];    //initilize
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = 0;
+        }
+         for(int j=0;j<n+1;j++){
+            dp[0][j] = 0;
+        }
+
+    for(int i=1;i<n+1;i++){                    //if both the char are same so reduce both length by -1
+        for(int j=1;j<m+1;j++){
+            if(str3.charAt(i-1) == str4.charAt(j-1)){
+                dp[i][j] = dp[i-1][j-1]+1;
+                ans = Math.max(ans, dp[i][j]);
+            }
+            else{
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return ans;
+  }
+
+
+
     public static void main(String[] args) {
    int coins[] ={1,2,3};
    int sum =4;
@@ -310,6 +339,12 @@ public class DynamicProgramming {
    int m = str2.length();
    int dp[][] = new int[n+1][m+1];
    System.out.println(lcsTab(str1, str2));
+
+
+   String str3 = "ABCDE";
+   String str4 = "ABGCE";
+   System.out.println(longestCommonSubstring(str3, str4));
+
 
 
 
