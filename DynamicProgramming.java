fc2c09ebb6394c6095bc8b70844d9a18c3b8e4d3
PriyaPandey19 <pp479021@gmail.com>
2025-06-23 19:19:01 +0530

    hy

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -212,8 +212,12 @@ public class DynamicProgramming {
     return dp[n][sum];
   }
 
+
+
+
+
   //weight => length , val->prices, W-> totRod
-  public static int rodCutting(int length[], int prices[], int totRod){
+  public static int rodCutting(int length[], int prices[], int totRod){   //rod cutting using memoization
 
     int n = prices.length;
     int dp[][] = new int[n+1][totRod+1];
@@ -241,6 +245,26 @@ public class DynamicProgramming {
 
 
 
+  public static int lcs2(String str1, String str2, int n, int m, int dp[][]){
+    if(n ==0 || m==0){
+        return 0;
+    }
+    if(dp[n][m] != -1){
+        return dp[n][m];
+    }
+
+    if(str1.charAt(n-1) == str2.charAt(m-1)){
+        return dp[n][m] = lcs2(str1, str2, n-1, m-1,dp)+1;
+    }
+    else{
+        int ans1 = lcs2(str1, str2, n-1, m, dp);
+        int ans2 =lcs2(str1, str2, n, m-1, dp);
+        return dp[n][m] = Math.max(ans1, ans2);
+    }
+  }
+
+
+
     public static void main(String[] args) {
    int coins[] ={1,2,3};
    int sum =4;
@@ -250,6 +274,20 @@ public class DynamicProgramming {
    int prices[] ={1,5,8,9,10,17,17,20};
    int totRod = 8;
    System.out.println(rodCutting(length, prices, totRod));
+
+   String str1 = "abcdge";
+   String str2 = "abedg";
+   int n = str1.length();
+   int m = str2.length();
+   int dp[][] = new int[n+1][m+1];
+
+   //intilization
+   for(int i=0;i<n+1;i++){
+    for(int j=0;j<m+1;j++){
+        dp[i][j] = -1;
+    }
+   }
+   System.out.println(lcs2(str1, str2, n, m, dp));
 
  
 
