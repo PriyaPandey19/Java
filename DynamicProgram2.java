665025b60890621540c57e91557d26f0216e9f21
PriyaPandey19 <pp479021@gmail.com>
2025-06-25 20:14:47 +0530

    dp

--- DynamicProgram2.java
+++ DynamicProgram2.java
@@ -100,6 +100,19 @@ public class DynamicProgram2 {
     }
 
 
+    public static int catalanTab(int n){
+    int dp[] = new int[n+1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<i;j++){
+            dp[i] += dp[j]* dp[i-j-1];
+        }
+    }
+    return dp[n];
+    }
+
+
 
 
 
@@ -117,5 +130,7 @@ public class DynamicProgram2 {
     int dp[] = new int[n+1];
     Arrays.fill(dp,-1);
     System.out.println(catalanMem(n, dp));
+
+    System.out.println(catalanTab(30));
     }
 }
