6aef5e92445b7d13e65de2ad951b204e27821397
PriyaPandey19 <pp479021@gmail.com>
2025-06-25 19:15:21 +0530

    dp

--- DynamicProgram2.java
+++ DynamicProgram2.java
@@ -32,9 +32,53 @@ public class DynamicProgram2 {
         return dp[n][m];
     }
 
+
+    public static boolean isMatch(String s, String p){
+        int n = s.length();
+        int m = p.length();
+
+        boolean dp[][] = new boolean[n+1][m+1];
+        //intilize
+        dp[0][0] = true;
+        //patter =" "
+        for(int i=1;i<n+1;i++){
+            dp[i][0] = false;
+        }
+        //s =" "
+        for(int  j=1;j<m+1;j++){
+            if(p.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+
+        //bootom up approach
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                //case ->ith char == jth char || jth char == ?
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+
+
     public static void main(String[] args) {
     String word1 ="intention";
     String word2 = "execution";
     System.out.println(editDistance(word1, word2));
+
+
+    String s = "abc";
+    String p = "**d";
+    System.out.println(isMatch(s, p));
     }
 }
