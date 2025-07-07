fc61be06bcb333de26fcb6e20b14dee875a44286
PriyaPandey19 <pp479021@gmail.com>
2025-07-07 20:59:03 +0530

    tracking

--- BackTracking.java
+++ BackTracking.java
@@ -371,23 +371,23 @@ public static boolean solveKTUtil(int x,int y, int movei,int sol[][],int xMove[]
         // }
 
       
-    //   int sudoku[][] ={{0,0,8,0,0,0,0,0,0},
-    //    {4,9,0,1,5,7,0,0,2},
-    //    {0,0,3,0,0,4,1,9,0},
-    //    {1,8,5,0,6,0,0,2,0},
-    //    {0,0,0,0,2,0,0,6,0},
-    //    {9,6,0,4,0,5,3,0,0},
-    //    {0,3,0,0,7,2,0,0,4},
-    //    {0,4,9,0,3,0,0,5,7},
-    //    {8,2,7,0,0,9,0,1,3} };
+      int sudoku[][] ={{0,0,8,0,0,0,0,0,0},
+       {4,9,0,1,5,7,0,0,2},
+       {0,0,3,0,0,4,1,9,0},
+       {1,8,5,0,6,0,0,2,0},
+       {0,0,0,0,2,0,0,6,0},
+       {9,6,0,4,0,5,3,0,0},
+       {0,3,0,0,7,2,0,0,4},
+       {0,4,9,0,3,0,0,5,7},
+       {8,2,7,0,0,9,0,1,3} };
 
-    // if(sudokuSolver(sudoku, 0, 0)){
-    //     System.out.println("solution exists");
-    //     printSudoku(sudoku);
-    // }
-    // else{
-    //     System.out.println("solution does not exists");
-    // }
+    if(sudokuSolver(sudoku, 0, 0)){
+        System.out.println("solution exists");
+        printSudoku(sudoku);
+    }
+    else{
+        System.out.println("solution does not exists");
+    }
 
 //     int maze[][] ={{1,0,0,0},
 //                    {1,1,0,1},
