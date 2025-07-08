6327e4c257e156912969e054579577c6a9148f71
PriyaPandey19 <pp479021@gmail.com>
2025-07-08 22:10:01 +0530

    backtracking

--- BackTracking.java
+++ BackTracking.java
@@ -353,7 +353,7 @@ public static boolean solveKTUtil(int x,int y, int movei,int sol[][],int xMove[]
 
 
 
-    public static void main(String[] args){
+    public static void main(String[] args){    //main function
         // int n = 4;
         // char board[][] = new char[n][n];
         // //initialize
