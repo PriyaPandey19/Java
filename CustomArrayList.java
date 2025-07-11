6df9ddbdaee5fc4b145aedf6757ebdbf9cb422ae
PriyaPandey19 <pp479021@gmail.com>
2025-07-11 22:18:45 +0530

    arraylist

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -156,12 +156,12 @@ public static void divideConque(int start,int increment,ArrayList<Integer> ans,i
 }
 
     public static void main(String args[]){
-        ArrayList<Integer> list= new ArrayList<>();
+       // ArrayList<Integer> list= new ArrayList<>();
         ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
 
-        // ArrayList<Integer> list = new ArrayList<>();
-        // list.add(1); list.add(2);
-        // mainlist.add(list);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1); list.add(2);
+        mainlist.add(list);
 
         // ArrayList<Integer> list2 = new ArrayList<>();
         // list2.add(3); list2.add(4);
