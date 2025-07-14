87a5ee905670fcdb4c61a5975f8b8185062fdf18
PriyaPandey19 <pp479021@gmail.com>
2025-07-14 21:53:26 +0530

    array

--- ArraysCC.java
+++ ArraysCC.java
@@ -391,13 +391,14 @@ public class ArraysCC {
 
     public static int[] intersect(int[] nums1, int[] nums2) {    //to find intersection of two  arrays
      HashMap<Integer, Integer> map = new HashMap<>();
-     for(int num : nums1){
+     for(int num : nums1){                                    //add all thenumbers of array 1 in hashmap
+        //if the number is already present in the map, increment its count
         map.put(num, map.getOrDefault(num, 0) +1);
      }   
 
      ArrayList<Integer> result= new ArrayList<>();
-     for(int num : nums2){
-        if(map.containsKey(num) && map.get(num) > 0){
+     for(int num : nums2){                  //for each number in array 2
+        if(map.containsKey(num) && map.get(num) > 0){   //if the number is present in the map and its count is greater than 0
             result.add(num);
             map.put(num, map.get(num) -1);
         }
