6d95aa80a15eaacc26d0abb41d3994306e8db728
PriyaPandey19 <pp479021@gmail.com>
2025-07-17 22:50:46 +0530

    array

--- ArraysCC.java
+++ ArraysCC.java
@@ -456,6 +456,29 @@ public class ArraysCC {
      return maxlength;
     }
 
+    public boolean canThreePartsEqualSum(int[] arr) {   //check if we can split the array into three parts with equal sum 
+     int total =0;
+     for(int num : arr){
+        total += num;
+     }   
+     if(total %3 != 0) return false;
+
+     int target = total/3;
+     int count =0;
+     int currentSum =0;
+
+     for(int i =0;i<arr.length;i++)
+     {
+        currentSum += arr[i];
+        if(currentSum == target){
+            count++;
+            currentSum =0;
+            if(count == 2 && i< arr.length -1) return true;
+        }
+     }
+     return false;
+    }
+
     
 
 
