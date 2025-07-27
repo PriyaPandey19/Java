3bf3a3ee6349a94aaf76983812e46c5db56660c5
PriyaPandey19 <pp479021@gmail.com>
2025-07-27 21:23:23 +0530

    array

--- ArraysCC.java
+++ ArraysCC.java
@@ -557,6 +557,20 @@ public class ArraysCC {
 
        return set.size() == map.size();
     }
+
+
+    public int heightChecker(int[] heights) {  //
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+
+        int count =0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i] != expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
     
 
 
