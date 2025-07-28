712a91e71aec8ccded8da72db8b649097dc73bcb
PriyaPandey19 <pp479021@gmail.com>
2025-07-28 20:33:19 +0530

    array

--- ArraysCC.java
+++ ArraysCC.java
@@ -572,6 +572,24 @@ public class ArraysCC {
         }
         return count;
     }
+
+
+      public int[] smallerNumbersThanCurrent(int[] nums) {   //find how many numbers are smaller than the current number
+       //we will use a nested loop to count the number of smaller numbers for each number
+       int n = nums.length;
+       int[] result = new int[n];
+
+       for(int i=0;i<n;i++){
+        int count =0;
+        for(int j=0;j<n;j++){
+            if(j != i && nums[j] < nums[i]){
+                count++;
+            }
+        }
+        result[i] = count;
+       } 
+       return result;
+    }
     
 
 
